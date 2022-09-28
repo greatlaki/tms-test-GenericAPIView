3f1e922fea2e2e5b9e746ddfab6e5087ca839da9
greatlaki <greatlaki911@gmail.com>
2022-09-28 14:18:56 +0300

    add CreateAPIView, ListAPIView instead ListModelMixin, CreateModelMixin, GenericAPIView

--- test_django/articles/views.py
+++ test_django/articles/views.py
@@ -1,21 +1,13 @@
-from rest_framework.generics import get_object_or_404
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework.generics import get_object_or_404, CreateAPIView, ListAPIView
 
 from articles.models import Article, Author
 from articles.serializers import ArticleSerializer
 
 
-class ArticleView(ListModelMixin, CreateModelMixin, GenericAPIView):
+class ArticleView(CreateAPIView, ListAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         author = get_object_or_404(Author, id=self.request.data.get('author_id'))
         return serializer.save(author=author)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
