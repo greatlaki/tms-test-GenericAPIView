33f2ed548f7a40fa4483ae327d7c6c68d9d474d9
greatlaki <greatlaki911@gmail.com>
2022-09-28 17:45:02 +0300

    RetrieveAPIView
    With the RetrieveAPIView class, we can view only one article using the article ID

--- test_django/articles/views.py
+++ test_django/articles/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import get_object_or_404, ListCreateAPIView
+from rest_framework.generics import get_object_or_404, ListCreateAPIView, RetrieveAPIView
 
 from articles.models import Article, Author
 from articles.serializers import ArticleSerializer
@@ -11,3 +11,8 @@ class ArticleView(ListCreateAPIView):
     def perform_create(self, serializer):
         author = get_object_or_404(Author, id=self.request.data.get('author_id'))
         return serializer.save(author=author)
+
+
+class SingleArticleView(RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
