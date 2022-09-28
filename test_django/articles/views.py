3eff34eb881088a53b251205a3fcc2e5244f63c0
greatlaki <greatlaki911@gmail.com>
2022-09-28 14:14:42 +0300

    post request

--- test_django/articles/views.py
+++ test_django/articles/views.py
@@ -1,7 +1,8 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin
 
-from articles.models import Article
+from articles.models import Article, Author
 from articles.serializers import ArticleSerializer
 
 
@@ -11,3 +12,10 @@ class ArticleView(ListModelMixin, GenericAPIView):
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        author = get_object_or_404(Author, id=self.request.data.get('author_id'))
+        return serializer.save(author=author)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
